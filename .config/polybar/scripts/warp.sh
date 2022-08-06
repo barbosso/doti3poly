8e55909e392aaf9d8440ee5d265e95749bf639f9
Barbosso <kalilinuxxx662@gmail.com>
2022-08-06 21:46:54 +0300

    add scripts

--- .config/polybar/scripts/warp.sh
+++ .config/polybar/scripts/warp.sh
@@ -1,6 +1,47 @@
 #!/bin/bash
-if [ $(warp-cli status | grep update | cut -d ' ' -f 3) == 'Connected' ];then 
-    warp-cli disconnect 1>/dev/null 2>&1
-else warp-cli connect 1>/dev/null 2>&1
-fi
-echo $(warp-cli status | grep update | cut -d ' ' -f 3)
+
+function status() {
+    if [ $(warp-cli status | grep update | cut -d ' ' -f 3) == 'Connected' ];then
+        echo "CON"
+    else echo "DISCON"
+    fi
+}
+
+function connect() {
+    if [ $(warp-cli status | grep update | cut -d ' ' -f 3) == 'Connected' ];then
+        warp-cli disconnect 1>/dev/null 2>&1
+    else warp-cli connect 1>/dev/null 2>&1
+    fi
+}
+
+function getOptVal() {
+    if [[ "$1" = *=* ]]; then
+        val="${1//*=/}"
+        return 1
+    fi
+
+    val="$2"
+}
+
+case $1 in
+    help)
+        usage
+        ;;
+    status)
+        status
+        ;;
+    connect)
+        connect
+        ;;
+esac
+
+
+
+
+# if [ $(warp-cli status | grep update | cut -d ' ' -f 3) == 'Connected' ];then 
+#     warp-cli disconnect 1>/dev/null 2>&1
+# else warp-cli connect 1>/dev/null 2>&1
+# fi
+
+
+# echo $(warp-cli status | grep update | cut -d ' ' -f 3)
